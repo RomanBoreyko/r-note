f8118022aaef020ee708b0e4479ce14251153136
R0 <68852309+R0-32@users.noreply.github.com>
2023-06-15 18:06:39 +0000

    MDW-Base1

---
+++ ДоХХХХоrm/4_core/MDW.cpp
@@ -0,0 +1,26 @@
+Lib "Udurgh";
+Lib "Templ";
+Lib "Decoration";
+Lib "Function";
+
+MorDomWorm () {
+    
+    Main() {
+        Udurgh U_Metropolis();
+
+    }
+
+    public Udurgh U_Metropolis () {
+        Decoration (
+            D_Fundament,
+            D_Overbuilding
+        ) {};
+        Templ T_Suit() {}
+        Udurgh U_Boddho() {}
+    }
+    
+    Function Presentation () {
+
+    }
+
+}
